7f4fc04610628be25cae769e03857a46e53d5e82
rmorgan2011 <morgan15@up.edu>
2016-02-10 22:50:52 -0800

    Added Flashlight Model, Sound Effects, and Level Mockup
    Looks like I'm cloning Slender.

--- Mockup/Assets/_Scripts/Flashlight.cs
+++ Mockup/Assets/_Scripts/Flashlight.cs
@@ -4,10 +4,32 @@ using System.Collections;
 public class Flashlight : MonoBehaviour {
 
     public GameObject cameraRotate;
+    private bool isOn;
 
+    void Start()
+    {
+        gameObject.GetComponent<AudioSource>().enabled = true;
+    }
 	void Update () {
 
         gameObject.transform.rotation = cameraRotate.transform.rotation;
+       
 
-	}
+        if (Input.GetMouseButtonUp(0))
+        {
+            isOn = !isOn;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+
+        if (isOn)
+        {
+            gameObject.GetComponent<Light>().enabled = true;
+            
+        }
+        else
+        {
+            gameObject.GetComponent<Light>().enabled = false;
+        }
+
+    }
 }
