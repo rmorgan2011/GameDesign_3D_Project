f0d7b70dd6778f8956f7c47b2ffb198d07e92b58
rmorgan2011 <morgan15@up.edu>
2016-02-10 18:36:15 -0800

    Initial Commit
    Basic Flashlight and Camera Implemented.

---
+++ Mockup/Assets/_Scripts/Flashlight.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+    public GameObject cameraRotate;
+
+	void Update () {
+
+        gameObject.transform.rotation = cameraRotate.transform.rotation;
+
+	}
+}
