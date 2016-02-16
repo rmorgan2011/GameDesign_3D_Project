5beec38af2f9cd7c1bc5d654b192907c96563c39
rmorgan2011 <morgan15@up.edu>
2016-02-15 17:47:15 -0800

    Added Battery Mechanic and Start of Sprint
    Battery Power is now implemented.

--- Mockup/Assets/_Scripts/Flashlight.cs
+++ Mockup/Assets/_Scripts/Flashlight.cs
@@ -5,31 +5,41 @@ public class Flashlight : MonoBehaviour {
 
     public GameObject cameraRotate;
     public bool isOn;
+    public bool canBeOn;
 
     void Start()
     {
         gameObject.GetComponent<AudioSource>().enabled = true;
+        canBeOn = true;
     }
 	void Update () {
 
         gameObject.transform.rotation = cameraRotate.transform.rotation;
-       
 
-        if (Input.GetMouseButtonUp(0))
+
+
+        if (canBeOn == true)
         {
-            isOn = !isOn;
-            gameObject.GetComponent<AudioSource>().Play();
+            if (Input.GetMouseButtonUp(0))
+            {
+                isOn = !isOn;
+                gameObject.GetComponent<AudioSource>().Play();
+            }
+        }
+        else
+        {
+            isOn = false;
         }
 
         if (isOn)
         {
             gameObject.GetComponent<Light>().enabled = true;
-            
         }
         else
         {
             gameObject.GetComponent<Light>().enabled = false;
         }
+        
 
     }
 }
---
+++ Mockup/Assets/_Scripts/GameController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject flashlight;
+    public GameObject flashlightText;
+    public GameObject sprintText;
+
+    public float maxSprint;
+    public float maxBattery;
+
+    public float sprintReduce;
+    public float batteryReduce;
+
+    private float currSprint;
+    private float currBattery;
+
+    private float batteryPercent;
+    private float sprintPercent;
+    
+
+    public bool isSprinting;
+    public bool isOn;
+
+    private bool sprintWait = false;
+    private bool flashlightWait = false;
+
+	// Use this for initialization
+    void Start()
+    {
+        currSprint = maxSprint;
+        currBattery = maxBattery;
+
+        batteryPercent = (currBattery / maxBattery) * 100;
+
+        isOn = flashlight.GetComponent<Flashlight>().isOn;
+        flashlightText.GetComponent<GUIText>().text = "Battery: " + batteryPercent + "%";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        while (isOn && currBattery > 0f && flashlightWait == false)
+        {
+            Debug.Log(currBattery);
+            StartCoroutine(reduceFlashlight());
+        }
+
+        if (currBattery == 0f)
+        {
+            flashlight.GetComponent<Flashlight>().canBeOn = false;
+        }
+        else
+        {
+            flashlight.GetComponent<Flashlight>().canBeOn = true;
+        }
+        isOn = flashlight.GetComponent<Flashlight>().isOn;
+       
+        batteryPercent = (currBattery / maxBattery) * 100;
+        flashlightText.GetComponent<GUIText>().text = "Battery: " + batteryPercent + "%";
+        
+       
+    }
+
+    IEnumerator reduceFlashlight()
+    {
+        flashlightWait = true;
+       
+        currBattery -= batteryReduce;
+        yield return new WaitForSeconds(2f);
+        flashlightWait = false;
+    }
+
+    IEnumerator reduceSprint()
+    {
+        sprintWait = true;
+        currSprint -= sprintReduce;
+        yield return new WaitForSeconds(2f);
+        sprintWait = false;
+    }
+
+
+}
