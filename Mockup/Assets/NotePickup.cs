eb66cdde5c129822ecbad675194ec3ee7b0a7271
Micah Alconcel <alconcel16@up.edu>
2016-02-28 15:23:59 -0800

    added very crude note pickups

---
+++ Mockup/Assets/NotePickup.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+using System.Collections;
+
+public class NotePickup : MonoBehaviour {
+	public GameObject noteCanvas;
+	public GameObject title;
+	public GameObject content;
+	public GameObject player;
+
+
+	//these two tell which note texts to display
+	//needs to be set for each different note you place
+	public string noteTitle;
+	public string noteContent;
+
+	private bool isVisible = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//allow player to press a button to dismiss the popup message
+		if (isVisible) {
+			if(Input.GetKeyDown(KeyCode.E)){
+				noteCanvas.SetActive (false);
+				player.GetComponent<FirstPersonController>().enabled = true;
+				Destroy (transform.gameObject);
+			}
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			noteCanvas.SetActive (true);
+			isVisible = true;
+			title.GetComponent<Text> ().text = noteTitle;
+			content.GetComponent<Text> ().text = noteContent;
+			player.GetComponent<FirstPersonController>().enabled = false;
+
+		}
+	}
+}
